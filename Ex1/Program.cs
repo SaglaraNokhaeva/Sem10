14111e3be226114dcada6f4275ae89f74adc8492
Saglara <nohaevass@yandex.ru>
2022-10-16 22:59:44 +0300

    64, 66 или 68

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-Console.Clear();
+﻿Console.Clear();
 Console.WriteLine("Введите номер задачи (64, 66 или 68) для проверки:");
 int num = int.Parse(Console.ReadLine());
 if (num == 64) Task64();
@@ -86,29 +83,26 @@ void Task68()
     int m = Convert.ToInt32(Console.ReadLine());
     Console.Write("Введите число n = ");
     int n = Convert.ToInt32(Console.ReadLine());
+    if (m > n) { int temp = m; m = n; n = temp; };
+    int NOD = m;
+    FindNOD(m, n);
 
-    NOD(m, n);
 
-    void NOD(int min, int max)
+    void FindNOD(int min, int max)
     {
-        if (min > max) { int temp = min; min = max; max = temp; }
-        int k = min;
-        if (k >= 1)
+        if (NOD >= 1)
         {
-            if ((max % k == 0) && (min % k == 0)) { Console.WriteLine(k); return; }
+            if ((max % NOD == 0) && (min % NOD == 0)) { Console.WriteLine(NOD); return; }
             else
             {
-                k--;
-                //min = k;
-                NOD(k, max);
-                //Console.Write(min);
+                NOD--;
+                //Console.WriteLine(k);
+                FindNOD(min, max);
             }
         }
         else
         {
-            Console.WriteLine("НОД = 1");
             return;
         }
     }
 };
-
